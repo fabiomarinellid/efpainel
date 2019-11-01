5015437743cd66e035e71a8887b7cf12190bf04d
Fabio Dionizio <fabio.dionizio@sialog.com.br>
2019-11-01 15:23:01 +0000

    Criação do controller para update dos items

---
+++ app/controllers/backoffice/product/item_controller.rb
@@ -0,0 +1,31 @@
+class Backoffice::Product::ItemController < ApplicationController
+  
+  #controller para corrigir conflito do update dos produtos e items
+
+  layout "backoffice"
+
+  def edit
+  	 set_produto
+  end
+
+  
+  def update
+  	set_produto
+    if @produto.update(params_produto)
+      redirect_to backoffice_product_product_index_path, notice: "Produto #{@produto.id} alterado com sucesso"
+    else
+      render :edit
+    end
+  end
+
+ 
+ private
+
+    def set_produto
+      @produto = Item.find(params[:id])
+    end
+
+    def params_produto
+        params.require(:item).permit(:name, :description, :price,  :active, :bestseller, :category_id, :label_id, :site_id)
+    end
+end
