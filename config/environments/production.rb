c15d6249f5dbf13b182e64ad96b8baf717a05522
Fabio Dionizio <fabio.dionizio@sialog.com.br>
2020-01-13 00:17:45 +0000

    js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += [/.*\.css/]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
