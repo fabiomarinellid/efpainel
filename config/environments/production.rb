34d7cf99c4f65250ebb1544dac4af88b7ab8cace
Fabio Dionizio <fabio.dionizio@sialog.com.br>
2020-01-13 00:34:23 +0000

    js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,12 +23,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
+  config.assets.precompile += %w( *.js *.css )
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
