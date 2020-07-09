0e1902b1c073e6fe678e0618d117cdaa04e873af
Fabio Marinelli <fabio.marinellidionizio@gmail.com>
2020-07-09 14:55:03 +0000

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/.
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
